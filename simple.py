761a35f5667a170a3971863aeade7e5278d688ad
pige252 <pige252@gmail.com>
2016-08-03 10:04:43 +0900

    add simple_adc

---
+++ simple.py
@@ -0,0 +1,26 @@
+import wiringpi as wp
+
+def read_adc(channel):
+    buff=[0x06,0,0x00]
+    adcValue=0
+    buff[1]=(channel&0x07)<<6
+
+    wp.digitalWrite(6,0)        #CS(6), False
+    wp.wiringPiSPIDataRW(0,buff,3)
+
+    buff[1]=0x0F&buff[1]
+    adcValue=(buff[1]<<8)|buff[2]
+
+    wp.digitalWrite(6,1)        #CS(6), True
+
+    return adcValue
+
+if wp.wiringPiSetup()==-1:
+    print "Unable to start wiringPi"
+
+if wp.wiringPiSPISetup(0,1000000)==-1:
+    print "wiringPiSPISetup Failed"
+
+while 1:
+    adcValue=read_adc(0)
+    print adcValue
